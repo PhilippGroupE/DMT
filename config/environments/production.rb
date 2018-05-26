0f9593649854c00f016df1187dc11b6c85f1fd26
philippgroupe <philippgroupe@gmail.com>
2018-05-26 12:26:12 +0200

    newly adjustments LP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,12 +25,16 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
+
+  # precompiling the .svg images
+  # config.assets.precompile += %w( '.svg')
+
+  # config.assets.css_compressor = :sass
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
